ca7fb6ffa4eae02ff07d28bd84b94f9c7d411f02
Jo-yu Feng <55816694+ReginaFeng@users.noreply.github.com>
2021-03-11 14:00:22 +0000

    Add files via upload

---
+++ game.js
@@ -0,0 +1,109 @@
+
+var buttonColours = ["red", "blue", "green", "yellow"];
+
+var gamePattern = [];
+var userClickedPattern = [];
+
+var gameStarted = false;
+var level = 0;
+
+
+$(document).keypress(function () { 
+    if(!gameStarted){
+        $("#level-title").text("Level " + level);
+        nextSequence();
+        gameStarted = true;
+    };
+});
+
+
+$(".btn").click(function () { 
+    var userChosenColour = $(this).attr("id");
+    // add id to the button got clicked
+    userClickedPattern.push(userChosenColour);
+    // console.log(userClickedPattern); > check the array
+
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+
+    checkAnswer(userClickedPattern.length-1);
+});
+
+
+function checkAnswer(currentLevel) {
+
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+
+      console.log("success");
+
+      if (userClickedPattern.length === gamePattern.length){
+
+        setTimeout(function () {
+          nextSequence();
+        }, 1000);
+
+      };
+
+    } else {
+
+        console.log("wrong");
+        playSound("wrong");
+        $("#level-title").text("Game Over, Press Any Key to Restart");
+        $("body").addClass("game-over");
+        setTimeout(function(){
+            $("body").removeClass("game-over")
+        }, 200);
+
+        startOver();
+    };
+
+};
+
+
+function nextSequence() {
+
+    userClickedPattern = [];
+
+    level++;
+
+    $("#level-title").text("Level " + level);
+
+    var randomNumber = Math.floor(Math.random() * 4);
+    // console.log(randomNumber);
+
+    var randomChosenColour = buttonColours[randomNumber];
+
+    // console.log(randomChosenColour);
+
+    gamePattern.push(randomChosenColour);
+
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
+    // var audio = new Audio("sounds/" + name + ".mp3");
+    // audio.play();
+    // Use nextSequence() in developetool for testing
+
+    playSound(randomChosenColour);
+
+};
+
+
+function playSound(name) {
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+    // Use nextSequence() in developetool for testing
+};
+
+
+function animatePress(currentColour) {
+    $("#" + currentColour).addClass("pressed");
+    setTimeout(function(){
+        $("#" + currentColour).removeClass("pressed")
+    },100);
+};
+
+function startOver(){
+    level = 0;
+    gamePattern = [];
+    gameStarted = false;
+};
